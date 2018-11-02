8b2b1bebb11cc5a644590aa5e93b3aa0195d1bf7
sudheerj <jsudheer50@gmail.com>
2018-11-02 09:28:55 +0800

    Initial commit of TodoList application

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,11 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
+    <div id="todo-panel">
     <router-view/>
+    </div>
   </div>
 </template>
 
@@ -13,17 +14,26 @@
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
+  width: 100%;
 }
 #nav {
   padding: 30px;
   a {
     font-weight: bold;
-    color: #2c3e50;
+    color: #736AFF;
     &.router-link-exact-active {
       color: #42b983;
     }
+    text-decoration: none;
   }
+  border-bottom: 1px solid;
+}
+
+#todo-panel {
+  width: 100%;
+  height: 100%;
+  background-color: cornflowerblue;
+
 }
 </style>
---
+++ src/views/TodoList.vue
@@ -0,0 +1,99 @@
+<template>
+  <div class="main">
+    <template>
+      <el-table :data="tableData.filter(data => !search || data.name.toLowerCase().includes(search.toLowerCase()))"  empty-text="No Items" class="wrapper">
+        <el-table-column prop="name" type="selection" style="width: 5px">
+
+        </el-table-column>
+        <el-table-column prop="name" width="750">
+          <template slot="header" slot-scope="slot">
+            <div class="wrapInput">
+            <el-input v-model="search" size="mini" style="width: 75%">
+            </el-input>
+            <el-button type="primary"
+                    size="mini"
+                    @click="handleAdd(scope.$index, scope.row)">Add</el-button>
+            </div>
+          </template>
+        </el-table-column>
+        <el-table-column prop="delete" >
+          <template slot-scope="scope">
+            <button class="destroy" @click="removeTodo(todo)"></button>
+          </template>
+        </el-table-column>
+
+      </el-table>
+    </template>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'todolist',
+  components: {
+    HelloWorld
+  },
+  data: function () {
+          return {
+              tableData: [{name: "sudheer"}, {name: "jonna"}],
+              search: '',
+      }
+  }
+}
+</script>
+<style>
+  .el-table_1_column_1.el-table-column--selection.is-leaf .cell {
+    display: none;
+  }
+
+  .destroy {
+    display: none;
+    position: absolute;
+    top: 0;
+    right: 10px;
+    bottom: 0;
+    width: 40px;
+    height: 40px;
+    margin: auto 0;
+    font-size: 30px;
+    color: #cc9a9a;
+    margin-bottom: 11px;
+    transition: color 0.2s ease-out;
+  }
+
+  .destroy:hover {
+    color: #af5b5e;
+  }
+
+  .destroy:after {
+    content: '×';
+  }
+
+  tr:hover .destroy {
+    display: block;
+  }
+
+  .wrapper {
+    border: 1px solid #ebeef5;
+  }
+
+  .main {
+    margin-left: 5%;
+    margin-right: 5%;
+    border-right: 1px solid #ebeef5;
+    height: 100%;
+  }
+
+  .wrapInput {
+    width: 100%;
+  }
+
+  .el-table.cell, .el-table th div {
+    padding-right: 10px;
+    overflow: visible !important;
+    text-overflow: ellipsis;
+  }
+</style>
